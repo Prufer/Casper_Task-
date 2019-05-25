81a1f8c5dfe43caf3a9b9c1fd31898edf6e9d9fb
SanithK <SanithK@DESKTOP-AJIH65A>
2019-05-25 14:03:01 +1000

    Add project files.

---
+++ Casper_Task/BaseClass/BaseTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casper_Task.BaseClass
+{
+    public class BaseTest
+    {
+
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Open()
+        {
+
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://beta.caspar-health.com/de/#/user/sign_in";
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("mat-input-0"))).SendKeys("IXE0865");
+
+            IWebElement PasswordText = driver.FindElement(By.XPath("//input[@id='mat-input-1']"));
+            PasswordText.SendKeys("78350619");
+
+            IWebElement SigninButton = driver.FindElement(By.XPath("/html/body/app-root/div/ui-view/ng-component/ui-view/app-sign-in/div/div[2]/form/div[3]/div/button/span"));
+            SigninButton.Click();
+            
+        }
+
+        //[OneTimeTearDown]
+        //public void Close()
+        //{
+        //    driver.Quit();
+        //}
+    }
+}
---
+++ Casper_Task/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using Casper_Task.BaseClass;
+using OpenQA.Selenium.Support.UI;
+namespace Casper_Task
+{
+    public class Program : BaseTest
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        [Test]
+        public void CreatePatient()
+        {
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[contains(text(),'Patienten anlegen')]"))).Click();
+
+            WebDriverWait wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            var FirstName = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@formcontrolname='first_name']")));
+            FirstName.Click();
+            FirstName.SendKeys("Sanith");
+
+            
+            IWebElement LastName = driver.FindElement(By.XPath("//*[@formcontrolname='last_name']"));
+            LastName.SendKeys("Kobe");
+
+            IWebElement date = driver.FindElement(By.XPath("//span[contains(.,'Tag')]"));
+            date.Click();
+            //var selectElement = new SelectElement(date);
+            //selectElement.SelectByIndex(4);
+            IWebElement DateDropDown = driver.FindElement(By.XPath("//mat-option[3]/span"));
+            DateDropDown.Click();
+
+
+            Boolean invisible = wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.XPath("//mat-option[3]/span")));
+
+            if (invisible)
+            {
+                IWebElement month = driver.FindElement(By.XPath("//span[contains(.,'Monat')]"));
+                month.Click();
+            }
+            IWebElement MonthDropDown = driver.FindElement(By.XPath("//mat-option[5]/span"));
+            DateDropDown.Click();
+      
+    
+
+            //IWebElement Year = driver.FindElement(By.XPath("//span[contains(.,'Jahr')]"));
+            //date.Click();
+            //IWebElement YearDropDown = driver.FindElement(By.XPath("//mat-option[1]/span"));
+            //DateDropDown.Click();
+
+            //IWebElement Country = driver.FindElement(By.XPath("//span[contains(.,'Land')]"));
+            //date.Click();
+            //IWebElement CountryDropDown = driver.FindElement(By.XPath("//mat-option[1]/span"));
+            //DateDropDown.Click();
+
+
+            driver.Quit();
+
+
+        }
+
+    }
+    }
+
